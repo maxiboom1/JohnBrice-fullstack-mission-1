29a11e0f2476ddef5f383eb79d6ff986f2570d21
maxiboom1 <galutziogen@gmail.com>
2022-11-15 11:33:45 +0200

    remove el and some style

--- assets/index.js
+++ assets/index.js
@@ -4,12 +4,13 @@ document.querySelector(".btn_clear").addEventListener('click', clear);
 
 let task_list = localStorage.length > 0 ? JSON.parse(localStorage.getItem('task_list')) : []; // ternary operator - very nice thing with condition on daclaration.
 
+render();
 function handleSubmit(event){
     event.preventDefault();
     const task = getFormData(event.target);
     //event.target.reset();
-    updateLocalStorage();
     tasklistUpdate(task);
+    updateLocalStorage();
     render();
 }
 
@@ -22,8 +23,18 @@ function getFormData(data){
     return task;
 }
 
-function tasklistUpdate(task){
-    task_list.push(task);
+function tasklistUpdate(task = undefined){
+    
+    let i =0;
+    for(;i<task_list.length;i++){
+        task_list[i].id = i; // set id to each task obj  
+    }
+    // on new task event only (wont run on remove task)
+    if(task){
+        task.id = i;
+        task_list.unshift(task);
+    }
+
 }
 
 function updateLocalStorage(){
@@ -36,24 +47,35 @@ function render(){
     for(let i = 0; i<task_list.length; i++){ 
         const node = document.createElement("div");
         node.classList.add("col-sm-4");
-        node.innerHTML = innerHTMLcreator(task_list[i]);
-        document.querySelector('.root').appendChild(node);
+        
+        if(i == 0) {node.classList.add("fade_in");
+            node.innerHTML = innerHTMLcreator(task_list[i]);
+            document.querySelector('.root').appendChild(node);
+        } else {
+            setTimeout(()=>{
+                node.innerHTML = innerHTMLcreator(task_list[i]);
+                document.querySelector('.root').appendChild(node);
+            },800)
+        }
     }   
 
 }
 
 function innerHTMLcreator(task){
-    console.log(task)
+    let listPointer = '*';
     let content = `
-    <div class="card">   
+    <div class="card" id="${task.id}">   
         <img src="assets/img/notebg.png" alt="" height="420" width="400">
-        <h3 class="card_header">${task.task_header}</h3>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-dash-circle-fill" onclick=(removeTask(this.parentElement)) viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z"/>
+        </svg>
+        <h1 class="card_header">${task.task_header}</h1>
         <ul class="card_list">
-            <li><input type="checkbox">${task.task_1}</li>
-            <li><input type="checkbox">${task.task_2}</li>
-            <li><input type="checkbox">${task.task_3}</li>
+            <li >${task.task_1 ? listPointer : ''} ${task.task_1}</li>
+            <li >${task.task_2 ? listPointer : ''} ${task.task_2}</li>
+            <li >${task.task_3 ? listPointer : ''} ${task.task_3}</li>
         </ul>
-        <h5 class="card_date">${task.date} ${task.time}</h5>
+        <h3 class="card_date">${task.date} ${task.time}</h3>
     </div>
     `
     return content;
@@ -63,4 +85,18 @@ function clear(){
     localStorage.clear();
     task_list.length = 0;
     render()
+}
+
+function removeTask(el){
+    const index = el.getAttribute("id")
+    task_list.splice(index,1);
+    
+    tasklistUpdate();
+    updateLocalStorage();
+    render();
+
+}
+
+function setSubmissionStatus(el){
+    console.log(el)
 }
